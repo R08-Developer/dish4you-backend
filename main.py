e5f0af641b240dff37530d9edc27f63bfa7da0c9
R08-Developer <r08_developer@proton.me>
2026-04-10 16:06:11 +0200

    Update main.py inclusie vertaling

--- main.py
+++ main.py
@@ -22,12 +22,15 @@ def import_recipe(data: ImportRequest):
 Analyseer deze tekst.
 
 1. Bepaal of dit een maaltijdrecept is.
-2. Extraheer titel, porties, ingrediënten en stappen.
-3. Haal hoeveelheden, eenheden, bereidingstijd en wachttijd eruit als die expliciet of heel duidelijk in de tekst staan.
-4. Geef aan wat ontbreekt.
-5. Geef waarschuwingen indien nodig.
+2. De invoer kan Nederlands, Duits of Engels zijn.
+3. Extraheer titel, porties, ingrediënten en stappen.
+4. Vertaal ALLE output naar het Nederlands.
+5. Haal hoeveelheden, eenheden, bereidingstijd en wachttijd eruit als die expliciet of heel duidelijk in de tekst staan.
+6. Geef aan wat ontbreekt.
+7. Geef waarschuwingen indien nodig.
 
 BELANGRIJK:
+- Ondersteun alleen Nederlands, Duits en Engels. Als de tekst een andere taal is, geef dit aan in warnings.
 - Verzín geen ontbrekende informatie.
 - Als iets ontbreekt: laat leeg of gebruik null.
 - Gebruik alleen tijden die echt in de tekst staan of duidelijk direct uit de tekst volgen.
@@ -35,10 +38,11 @@ BELANGRIJK:
 - waitMinutes = wachttijd / oven / rijzen / rusten / marineren enz.
 - title mag je voorzichtig afleiden indien duidelijk.
 - servings mag alleen ingevuld worden als het expliciet genoemd wordt.
+- Ingrediënten, stappen en titel moeten altijd in het Nederlands teruggegeven worden.
+- Engelse of buitenlandse eenheden mogen behouden blijven, maar geef een waarschuwing als ze mogelijk onduidelijk zijn (bijvoorbeeld 'cup', 'ounce').
 
 Tekst:
-{data.text}
-""",
+{data.text}""",
         text={
             "format": {
                 "type": "json_schema",
